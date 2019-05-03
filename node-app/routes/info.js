6deef7013b97ea93376842686da04610b27e65fd
Sumanth Kumar Mora <sumantkm@thoughtworks.com>
2019-05-03 21:40:59 +0530

    Add sample pod config for node-app

--- node-app/routes/info.js
+++ node-app/routes/info.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.status(200).send({ environment_var: process.env.ENV_PASSWORD });
+    res.status(200).send({
+        environment_var_username: process.env.ENV_USERNAME,
+        environment_var_password: process.env.ENV_PASSWORD,
+        environment_var_service_url: process.env.ENV_SERVICE_URL
+    });
 });
 
 module.exports = router;
